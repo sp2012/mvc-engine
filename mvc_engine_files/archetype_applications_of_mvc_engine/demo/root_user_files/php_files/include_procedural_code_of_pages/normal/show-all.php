38091ddb4322e730dc898a5e4bffa2aa4d6a58ba
Solon Papageorgiou <s.papageorgiou@mail.com>
2013-07-19 00:31:12 +0300

    echoing "TEST!!". Just trying to get total commander to compare the mvc_engines.

--- mvc_engine_files/archetype_applications_of_mvc_engine/demo/root_user_files/php_files/include_procedural_code_of_pages/normal/show-all.php
+++ mvc_engine_files/archetype_applications_of_mvc_engine/demo/root_user_files/php_files/include_procedural_code_of_pages/normal/show-all.php
@@ -1,4 +1,35 @@
 <?php
 
 
+
+
+
+
+function generateRandomCode($length){
+
+
+    $user_class_object = new UserClass();
+
+    return $user_class_object->generateRandomCode($length);
+
+}
+
+
+
+$general_object->content_for_pages['object_phplivex']->Ajaxify ("generateRandomCode");
+
+
+
+$general_object->content_for_pages['object_phplivex']->Run(); // Must be called inside the 'body' tag
+
+
+
 $ViewMain_object->output_page_show_all();
+
+
+echo "TEST!!";
+
+
+
+
+
